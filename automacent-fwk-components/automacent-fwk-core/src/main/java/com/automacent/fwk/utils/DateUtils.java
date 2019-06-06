f79463d027cf3fd60a4f343b5c33edd0b8d16e34
Sighil Sivadas <sighil.sivadas@gmail.com>
2019-06-06 05:57:20 +0000

    Add TestExecutor to execute tests from the command line

--- automacent-fwk-components/automacent-fwk-core/src/main/java/com/automacent/fwk/utils/DateUtils.java
+++ automacent-fwk-components/automacent-fwk-core/src/main/java/com/automacent/fwk/utils/DateUtils.java
@@ -3,6 +3,8 @@ package com.automacent.fwk.utils;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import com.automacent.fwk.execution.TestExecutor;
+
 /**
  * Util class related to date formatting
  * 
@@ -12,6 +14,8 @@ public class DateUtils {
 
 	private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MMM-dd HH:mm:ss,SSS");
 
+	private static final SimpleDateFormat suiteNameSdf = new SimpleDateFormat("yyyy-MMM-dd-HH-mm-ss");
+
 	/**
 	 * Get date in the date format yyyy-MMM-dd HH:mm:ss,SSS
 	 * 
@@ -19,5 +23,15 @@ public class DateUtils {
 	 */
 	public static String getDate() {
 		return sdf.format(new Date());
+	}
+
+	/**
+	 * Get date in the date format yyyy-MMM-ddTHH:mm:ss for setting up the suite
+	 * name in {@link TestExecutor}
+	 * 
+	 * @return String representing date
+	 */
+	public static String getDateFormattedForSuiteName() {
+		return suiteNameSdf.format(new Date());
 	}
 }
