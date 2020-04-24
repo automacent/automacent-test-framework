2e2a524f111976c16fc6b236cb46e89dc70003cb
Sighil Sivadas <ssivadas@vmware.com>
2020-04-24 13:49:02 +0000

    #27 Add interface to send logs to external server
    Added the following properties to ILauncherClient
    
    * Step start
    * Step fail and duration
    * Step pass and duration
    * Action start
    * Action fail and duration
    * Action pass and duration
    * Iteration start
    * Iteration fail and duration
    * Iteration pass and duration
    * Method start
    * Method fail and duration
    * Method pass and duration
    * Sleep start
    * Sleep end and duration

--- automacent-fwk-components/automacent-fwk-core/src/main/java/com/automacent/fwk/launcher/ILauncherClient.java
+++ automacent-fwk-components/automacent-fwk-core/src/main/java/com/automacent/fwk/launcher/ILauncherClient.java
@@ -4,6 +4,8 @@ import org.testng.IInvokedMethod;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
 
+import com.automacent.fwk.annotations.Action;
+import com.automacent.fwk.annotations.Step;
 import com.automacent.fwk.core.TestObject;
 import com.automacent.fwk.enums.MethodType;
 import com.automacent.fwk.exceptions.LauncherForceCompletedException;
@@ -79,4 +81,111 @@ public interface ILauncherClient {
 	 *             when test instance status is not RUNNING
 	 */
 	void ping() throws LauncherForceCompletedException;
+
+	/**
+	 * Log {@link Step} start
+	 * 
+	 * @param method
+	 */
+	public void logStepStart(String method);
+
+	/**
+	 * Log {@link Step} completion and duration
+	 * 
+	 * @param method
+	 * @param duration
+	 */
+	public void logStepPass(String method, long duration);
+
+	/**
+	 * Log {@link Step} failure and duration
+	 * 
+	 * @param method
+	 * @param duration
+	 */
+	public void logStepFail(String method, long duration);
+
+	/**
+	 * Log {@link Action} start
+	 * 
+	 * @param method
+	 */
+	public void logActionStart(String method);
+
+	/**
+	 * Log {@link Action} completion and duration
+	 * 
+	 * @param method
+	 * @param duration
+	 */
+	public void logActionPass(String method, long duration);
+
+	/**
+	 * Log {@link Action} failure and duration
+	 * 
+	 * @param method
+	 * @param duration
+	 */
+	public void logActionFail(String method, long duration);
+
+	/**
+	 * Log iteration start
+	 * 
+	 * @param method
+	 */
+	public void logIterationStart(String method);
+
+	/**
+	 * Log iteration completion and duration
+	 * 
+	 * @param method
+	 * @param duration
+	 */
+	public void logIterationPass(String method, long duration);
+
+	/**
+	 * Log iteration failure and duration
+	 * 
+	 * @param method
+	 * @param duration
+	 */
+	public void logIterationFail(String method, long duration);
+
+	/**
+	 * Log method start
+	 * 
+	 * @param method
+	 */
+	public void logMethodStart(String method);
+
+	/**
+	 * Log method completion and duration
+	 * 
+	 * @param method
+	 * @param duration
+	 */
+	public void logMethodPass(String method, long duration);
+
+	/**
+	 * Log method failure and duration
+	 * 
+	 * @param method
+	 * @param duration
+	 */
+	public void logMethodFail(String method, long duration);
+
+	/**
+	 * Log sleep start
+	 * 
+	 * @param method
+	 */
+	public void logSleepStart(String method);
+
+	/**
+	 * Log sleep completion and duration
+	 * 
+	 * @param method
+	 * @param duration
+	 */
+	public void logSleepEnd(String method, long duration);
 }
