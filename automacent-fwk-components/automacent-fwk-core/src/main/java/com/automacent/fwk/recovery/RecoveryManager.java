4ce4a9d87289bf3b77afdf3a9f6f0e359c49be98
Sighil Sivadas <sighil.sivadas@gmail.com>
2020-04-24 17:11:38 +0000

    #27 Call LauncherClient execution logs in framework
    Call logStart() and logEnd() at all the required locations

--- automacent-fwk-components/automacent-fwk-core/src/main/java/com/automacent/fwk/recovery/RecoveryManager.java
+++ automacent-fwk-components/automacent-fwk-core/src/main/java/com/automacent/fwk/recovery/RecoveryManager.java
@@ -1,13 +1,17 @@
 package com.automacent.fwk.recovery;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.automacent.fwk.annotations.StepsAndPagesProcessor;
+import com.automacent.fwk.enums.MethodType;
+import com.automacent.fwk.enums.TestStatus;
 import com.automacent.fwk.exceptions.RecoveryFailedException;
 import com.automacent.fwk.execution.IterationManager;
 import com.automacent.fwk.launcher.LauncherClientManager;
 import com.automacent.fwk.reporting.Logger;
+import com.automacent.fwk.utils.LoggingUtils;
 
 /**
  * This class manages the Recovery Scenarios for the test. If a test fails and
@@ -71,12 +75,20 @@ public class RecoveryManager {
 				&& IterationManager.getManager().getIteration() > 1) {
 			for (Class<?> recoveryClass : getRecoveryClasses()) {
 				_logger.info(String.format("Starting recovery steps specified in %s", recoveryClass.getName()));
+				long startTime = new Date().getTime();
+				String recoveryClassName = LoggingUtils.addSpaceToCamelCaseString(recoveryClass.getSimpleName())
+						.toLowerCase();
 				try {
+					LauncherClientManager.getManager().logStart(recoveryClassName, MethodType.RECOVERY);
 					WebRecovery recovery = (WebRecovery) recoveryClass.newInstance();
 					StepsAndPagesProcessor.processAnnotation(recovery);
 					recovery.checkRecoveryParameters();
 					recovery.recover();
+					LauncherClientManager.getManager().logEnd(recoveryClassName, MethodType.RECOVERY,
+							TestStatus.PASS, new Date().getTime() - startTime);
 				} catch (Throwable e) {
+					LauncherClientManager.getManager().logEnd(recoveryClassName, MethodType.RECOVERY,
+							TestStatus.FAIL, new Date().getTime() - startTime);
 					throw new RecoveryFailedException(e);
 				}
 				_logger.info(String.format("Recovery steps specified in %s completed", recoveryClass));
