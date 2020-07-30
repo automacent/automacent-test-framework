6655e9cb7c83d3c9664366ba0b2b3aa909bbcc95
Sighil Sivadas <sighil.sivadas@gmail.com>
2020-07-30 15:03:01 +0530

    #36 Add new Custom Expected condition waitTillTextInElementNotMatches

--- automacent-fwk-components/automacent-fwk-core/src/main/java/com/automacent/fwk/selenium/CustomExpectedConditions.java
+++ automacent-fwk-components/automacent-fwk-core/src/main/java/com/automacent/fwk/selenium/CustomExpectedConditions.java
@@ -1,5 +1,7 @@
 package com.automacent.fwk.selenium;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,6 +9,9 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.automacent.fwk.annotations.Action;
+import com.automacent.fwk.core.BaseTest;
 
 /**
  * Custom Expected conditions to be used with {@link WebDriverWait}
@@ -21,20 +26,25 @@ public class CustomExpectedConditions {
 	 * In case of using {@link FindBy} to locate elements, there is no
 	 * {@link ExpectedConditions} to override the ImplicitWait set and use
 	 * {@link WebDriverWait}. The custom proxyElementLocated
-	 * {@link CustomExpectedConditions#proxyElementLocated(WebElement)} will take
+	 * {@link CustomExpectedConditions#waitTillproxyElementLocated(WebElement)} will take
 	 * care of this scenario
 	 * 
 	 * @param proxyElement Proxy {@link WebElement} object
 	 * @return null if not found, element if found
 	 */
-	public static ExpectedCondition<WebElement> proxyElementLocated(final WebElement proxyElement) {
+	@Action
+	public static ExpectedCondition<WebElement> waitTillproxyElementLocated(final WebElement proxyElement) {
 		return new ExpectedCondition<WebElement>() {
 			@Override
 			public WebElement apply(WebDriver driver) {
+				driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
 				try {
 					proxyElement.getTagName();
 				} catch (NoSuchElementException e) {
 					return null;
+				} finally {
+					driver.manage().timeouts().implicitlyWait(BaseTest.getTestObject().getTimeoutInSeconds(),
+							TimeUnit.SECONDS);
 				}
 				return proxyElement;
 			}
@@ -42,6 +52,29 @@ public class CustomExpectedConditions {
 			@Override
 			public String toString() {
 				return "Proxy element to be locate in DOM";
+			}
+		};
+	}
+
+	@Action
+	public static ExpectedCondition<WebElement> waitTillTextInElementNotMatches(final WebElement proxyElement, String text) {
+		return new ExpectedCondition<WebElement>() {
+			@Override
+			public WebElement apply(WebDriver driver) {
+				driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+				try {
+					return !proxyElement.getText().equals("") ? proxyElement : null;
+				} catch (NoSuchElementException e) {
+					return null;
+				} finally {
+					driver.manage().timeouts().implicitlyWait(BaseTest.getTestObject().getTimeoutInSeconds(),
+							TimeUnit.SECONDS);
+				}
+			}
+
+			@Override
+			public String toString() {
+				return "Text in Element Not Matches given value";
 			}
 		};
 	}
