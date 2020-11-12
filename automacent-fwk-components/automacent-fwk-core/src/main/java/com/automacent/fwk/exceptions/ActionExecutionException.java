03aa3b8eef1f00ccd17a980ba6b0ae2ea371ee95
Sighil Sivadas <sighil.sivadas@gmail.com>
2020-11-12 15:45:19 +0530

    Get action and Step Method From Action and Step ExecutionException #50

--- automacent-fwk-components/automacent-fwk-core/src/main/java/com/automacent/fwk/exceptions/ActionExecutionException.java
+++ automacent-fwk-components/automacent-fwk-core/src/main/java/com/automacent/fwk/exceptions/ActionExecutionException.java
@@ -15,7 +15,19 @@ public class ActionExecutionException extends RuntimeException {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private String methodName;
+
 	public ActionExecutionException(String methodName, Throwable cause) {
 		super(String.format("[@Action %s] %s", methodName, cause.getMessage()), cause);
+		this.methodName = methodName;
+	}
+
+	/**
+	 * Get the method name where the failure occurred
+	 * 
+	 * @return Method name
+	 */
+	public String getMethodName() {
+		return methodName;
 	}
 }
--- automacent-fwk-components/automacent-fwk-core/src/main/java/com/automacent/fwk/exceptions/StepExecutionException.java
+++ automacent-fwk-components/automacent-fwk-core/src/main/java/com/automacent/fwk/exceptions/StepExecutionException.java
@@ -14,7 +14,20 @@ public class StepExecutionException extends RuntimeException {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private String methodName;
+
 	public StepExecutionException(String methodName, Throwable cause) {
 		super(String.format("[@Step %s] %s", methodName, cause.getMessage()), cause);
+		this.methodName = methodName;
 	}
+
+	/**
+	 * Get the method name where the failure occurred
+	 * 
+	 * @return Method name
+	 */
+	public String getMethodName() {
+		return methodName;
+	}
+
 }
